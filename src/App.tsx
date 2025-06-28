75621980fc53452d73592dc7e312f1d930062ee9
Ömer Balyalı <omerbalyali@gmail.com>
2025-06-28 13:42:19 +0200

    Start repository

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white">
+      {/* Your app content will go here */}
+    </div>
+  );
+}
+
+export default App;
